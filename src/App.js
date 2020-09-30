f26ea0e40e9841ad2cfa7c1dabb65bf55db963b5
Gabriela Schlemper <gabschlemper@gmail.com>
2020-09-30 15:34:33 -0300

    Created header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App"></div>
+    <>
+      <Header />
+      
+      <GlobalStyle />
+    </>
   );
 }
 
